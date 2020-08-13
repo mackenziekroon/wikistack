5c25146a92526a3f0475707cd7f4ac42cf43a7bc
Mackenzie Kroon <kroonmackenzie@gmail.com>
2020-08-13 14:04:12 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const morgan = require("morgan");
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const layout = require('./views/layout.js')
+const { db, Page, User } = require('./models');
+const userRouter = require('./routes/users')
+const wikiRouter = require('./routes/wiki')
+
+app.use(express.static(__dirname + "/public"))
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use('/wiki', wikiRouter);
+app.use('/users', userRouter);
+
+app.get('/', (req, res, next) => {
+    res.send(layout(''))
+})
+
+let PORT = 3000;
+
+const init = async () => {
+    await db.sync();
+    await db.sync({force: true})
+    // make sure that you have a PORT constant
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}!`);
+    });
+  }
+  
+  init();
